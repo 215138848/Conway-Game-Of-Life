552dccebbe9e8985227c9840a9b12cc39c75b15f
215138848 <215138848@mycput.ac.za>
2021-07-21 15:58:00 +0200

    Game of life works

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,52 +1,116 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args )    {
-        System.out.println( "Hello World!" );
-        String [][] board = {{"_","#","#","_","_"}, {"_","#","_","#","_"}, {"_","#","#","_","#"} };
-        String [] map = {"_#_", "_#_", "_#_"};
-        int rows = 3;
 
-        for(int i=0; i<rows; i++){
+    public static void main( String[] args )    {
+        System.out.println( "Welcome to Conway's Game of Life." );
+        Scanner scan = new Scanner(System.in);
+        System.out.println( "Please enter the size of the board?" );
+        int size = scan.nextInt();
+        System.out.println( "Ready?" );
+        String [][] board = GenNewBoard(size);
+        System.out.println( "Set?" );
+        
+        //String [][] board = {{"_","#","#","_","_"}, {"_","#","_","#","_"}, {"_","#","#","_","#"} };
+
+        for(int i=0; i<size; i++){
+            System.out.println( "Go!!!" );
             System.out.println("Generation " + i + ":");
-            System.out.println(Arrays.toString(map));
-            map = life(map);
+            //printing board
+            for (int rows =0; rows<size; rows++){
+                for (int columns=0; columns<size; columns++){
+                    System.out.print(board[rows][columns] + " ");
+                }
+                System.out.println("");
+            }
+            board = life(board);
         }
     }
 
-    public static String[] life(String [] map){
-        String [] newMap = new String[map.length];
+    public static String[][] life(String [][] board){
+        int rows = board.length;
+        int columns = board.length;
 
-        for (int row =0; row< map.length; row++){
-            newMap[row]="";
-            for (int i=0; i<map.length; i++ ){ /*i = colum*/
-                String above = "";
-                String same = "";
-                String below = "";
+        String [][] newBoard = new String[rows][columns];
 
-                if(i ==0){
-                    above = (map[row].substring(i));
+        for (int row =0; row< board.length; row++){
+
+            for (int i=0; i<board.length; i++ ){ /*i = columns*/
+
+                if(board[row][i] =="#"){
+                    //if space is populated
+                    int neighbourCount = neighbourCounter(board, row, i);
+                    if(neighbourCount<=1){
+                        newBoard[row][i] = "_"; //is dead
+                    }
+                    if(neighbourCount==2 || neighbourCount==3 ){
+                        newBoard[row][i] = "#"; //is alive
+                    }
+                    if(neighbourCount>=4 ){
+                        newBoard[row][i] = "_"; //is dead
+                    }
+                }
+                if(board[row][i] =="_"){
+                    //if space is unpopulated
+                    int neighbourCount = neighbourCounter(board, row, i);
+                    if(neighbourCount==3 ){
+                        newBoard[row][i] = "#"; //is alive
+                    }
+                    else {
+                        newBoard[row][i] = "_"; //is dead
+                    }
 
                 }
-                System.out.println(above);
 
             }
         }
 
-        return newMap;
+        return newBoard;
     }
 
-    public static String [][] GenNewBoard(String [][] board){
+    public static String [][] GenNewBoard(int size){
         String [][] newBoard;
 
-        newBoard = new String [][] {{"_","#","#","_","_"}, {"_","#","_","#","_"}, {"_","#","#","_","#"} };
+        newBoard = new String [][] {{"_","#","#","_","_"}, {"_","#","_","#","_"}, {"_","#","_","#","_"}, {"_","#","_","#","_"}, {"_","#","#","_","#"} };
 
         return newBoard;
+    }
+
+    public static int neighbourCounter(String [][] board, int x, int y){
+        int neighbourCount = 0;
+        if(y!=0){
+            if(x!=0){
+                if(board[x-1][y-1] == "#"){neighbourCount++;}           //Start at bottom left
+            }
+            if(board[x][y-1] == "#"){neighbourCount++;}
+            if(x!=board.length-1){
+                if(board[x+1][y-1] == "#"){neighbourCount++;}
+                            }
+        }
+        if(x!=0){
+            if(board[x-1][y] == "#"){neighbourCount++;}
+        }
+        //if(board[x][y] == "#"){neighbourCount++;}     must not count the original
+        if(x!=board.length-1){
+            if(board[x+1][y] == "#"){neighbourCount++;}
+        }
+        if(y!=board.length-1){
+            if(x!=0){
+                if(board[x-1][y+1] == "#"){neighbourCount++;}
+            }
+            if(board[x][y+1] == "#"){neighbourCount++;}
+            if(x!=board.length-1){
+                if(board[x+1][y+1] == "#"){neighbourCount++;}           //End at top right
+            }
+        }
+
+        return neighbourCount;
     }
 }
